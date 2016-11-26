a02ef8b007478714bfbcd683818bc71500574bd2
Ahtthapon <Ahtthapon@live.com>
2016-11-26 17:25:49 +0700

    Initial commit

---
+++ application/views/home_view.php
@@ -0,0 +1,86 @@
+
+
+<div class="container">
+    <h1>Page Heading
+        <small>Secondary Text</small>
+    </h1>
+    <hr class="divider">
+    <div class="row ">
+        <div class="col-xs-12 col-sm-4">
+            <div class="thumbnail">
+                <img src="http://placehold.it/700x300" alt="">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8">
+            <div class="caption">
+                <h2>Project One</h2>
+                <h4>Subheader</h4>
+                <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid
+                    aperiam asperiores
+                    distinctio eos et excepturi illum inventore laboriosam nihil nulla perferendis quae quasi qui
+                    quisquam recusandae repellat sint temporibus!</p>
+                <p><a href="#" class="btn btn-primary animated infinite tada" role="button"><i
+                            class="fa fa-firefox fa-spin fa-3x" aria-hidden="true"></i> View Project <span
+                            class="glyphicon glyphicon-chevron-right"></span></a></p>
+            </div>
+        </div>
+    </div>
+    <hr class="divider">
+    <div class="row ">
+        <div class="col-xs-12 col-sm-4">
+            <div class="thumbnail">
+                <img src="http://placehold.it/700x300" alt="">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8">
+            <div class="caption">
+                <h2>Project One</h2>
+                <h4>Subheader</h4>
+                <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid
+                    aperiam asperiores
+                    distinctio eos et excepturi illum inventore laboriosam nihil nulla perferendis quae quasi qui
+                    quisquam recusandae repellat sint temporibus!</p>
+                <p><a href="#" class="btn btn-primary animated infinite tada" role="button"><i
+                            class="fa fa-firefox fa-spin fa-3x" aria-hidden="true"></i> View Project <span
+                            class="glyphicon glyphicon-chevron-right"></span></a></p>
+            </div>
+        </div>
+    </div>
+    <hr class="divider">
+    <div class="row ">
+        <div class="col-xs-12 col-sm-4">
+            <div class="thumbnail">
+                <img src="http://placehold.it/700x300" alt="">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8">
+            <div class="caption">
+                <h2>Project One</h2>
+                <h4>Subheader</h4>
+                <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid
+                    aperiam asperiores
+                    distinctio eos et excepturi illum inventore laboriosam nihil nulla perferendis quae quasi qui
+                    quisquam recusandae repellat sint temporibus!</p>
+                <p><a href="#" class="btn btn-primary animated infinite tada" role="button"><i
+                            class="fa fa-firefox fa-spin fa-3x" aria-hidden="true"></i> View Project <span
+                            class="glyphicon glyphicon-chevron-right"></span></a></p>
+            </div>
+        </div>
+    </div>
+    <hr class="divider">
+    <div class="col-md-12">
+        <div class="col-md-offset-5">
+            <nav aria-label="...">
+                <ul class="pagination">
+                    <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                    <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
---
+++ application/views/test_view.php
@@ -0,0 +1,10 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea libero magni porro quam ullam! Consectetur doloremque
+    et excepturi hic minus nulla, obcaecati? Animi commodi dolorum ipsam minus neque omnis recusandae.
+</div>
+<div>Ducimus porro praesentium suscipit vel! A ab aut autem, consequuntur corporis deserunt est facilis hic in iste
+    libero magnam modi nemo provident quisquam quod quos sint tempora temporibus vitae voluptas!
+</div>
+<div>A accusamus cupiditate, et id ipsam nihil quam quas qui, quidem, tempore unde voluptatum? Adipisci deleniti eveniet
+    expedita minima modi praesentium temporibus. Atque eveniet fugiat laudantium nulla perspiciatis praesentium,
+    reprehenderit.
+</div>
